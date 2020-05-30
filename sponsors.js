bd9837f316b1f92cc8d5f4bd4d5dec05ac6e2702
Jennifer <jenniferbriere@gmail.com>
2020-05-29 18:38:15 -0700

    add new sponsor working

--- sponsors.js
+++ sponsors.js
@@ -1,11 +1,11 @@
-module.exports = function(){
+module.exports = function () {
     var express = require('express');
     var router = express.Router();
 
     /* get Sponsors to populate in dropdown */
-    function getSponsors(res, mysql, context, complete){
-        mysql.pool.query("SELECT sponsor_id, first_name, last_name FROM sponsors", function(error, results, fields){
-            if(error){
+    function getSponsors(res, mysql, context, complete) {
+        mysql.pool.query("SELECT sponsor_id, first_name, last_name FROM sponsors", function (error, results, fields) {
+            if (error) {
                 res.write(JSON.stringify(error));
                 res.end();
             }
@@ -13,24 +13,23 @@ module.exports = function(){
             complete();
         });
     }
-    /* Adds a new sponsor, then reloads the page after adding */
-    router.post('/', function(req, res){
+    /* Adds a new sponsor, then reloads the sponsorships page after adding */
+    router.post('/', function (req, res) {
         console.log(req.body.sponsors)
-        console.log(req.body.animals)        
         console.log(req.body)
         var mysql = req.app.get('mysql');
-        var sql = "INSERT INTO sponsorships (sponsor_id, date_of_sponsorship, animal_id, amount) VALUES (?,?,?,?)";
-        var inserts = [req.body.sid, req.body.date_of_sponsorship, req.body.aid, req.body.amount];
-        sql = mysql.pool.query(sql,inserts,function(error, results, fields){
-            if(error){
+        var sql = "INSERT INTO sponsors (first_name, last_name, phone_number, email) VALUES (?,?,?,?)";
+        var inserts = [req.body.first_name, req.body.last_name, req.body.phone_number, req.body.email];
+        sql = mysql.pool.query(sql, inserts, function (error, results, fields) {
+            if (error) {
                 console.log(JSON.stringify(error))
                 res.write(JSON.stringify(error));
                 res.end();
-            }else{
+            } else {
                 res.redirect('/sponsorships');
             }
         });
-    });    
+    });
 
     return router;
 }();
