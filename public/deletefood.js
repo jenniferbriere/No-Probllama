0c60bdb204ab23b128bd48f1a43f0d6cadb94b78
jordancolbeth <47009671+jordancolbeth@users.noreply.github.com>
2020-05-30 19:39:41 -0600

    Update deletefood.js

--- public/deletefood.js
+++ public/deletefood.js
@@ -1,1 +1,25 @@
 // more to come...
+function deleteFood(id){
+    $.ajax({
+        url: '/foods/' + id,
+        type: 'DELETE',
+        success: function(result){
+            window.location.reload(true);
+        }
+    })
+};
+
+function deleteFeeding(animal_id, food_id){
+  $.ajax({
+      url: '/feedings/animal/' + animal_id + '/food/' + food_id,
+      type: 'DELETE',
+      success: function(result){
+          if(result.responseText != undefined){
+            alert(result.responseText)
+          }
+          else {
+            window.location.reload(true)
+          } 
+      }
+  })
+};
