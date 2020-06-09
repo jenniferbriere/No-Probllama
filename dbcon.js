5d797ad7204013465428f8001f68697df0507820
Jennifer <jenniferbriere@gmail.com>
2020-06-08 17:13:41 -0700

    removed username and pw

--- dbcon.js
+++ dbcon.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
-  connectionLimit : 10,
-  host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_colbethj',
-  password        : '3329',
-  database        : 'cs340_colbethj'
+  connectionLimit: 10,
+  host: 'classmysql.engr.oregonstate.edu',
+  user: '',
+  password: '',
+  database: 'cs340_brierej'
 });
 module.exports.pool = pool;
